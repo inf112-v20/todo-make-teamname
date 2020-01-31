e9c78793371725e46b1cfb9d661b858ee9388e31
KrisAesoey <kris.aesoey@gmail.com>
2020-01-31 16:03:23 +0100

    Added a JUnit Test class for Board testing

---
+++ src/test/java/inf112/skeleton/app/BoardTests.java
@@ -0,0 +1,12 @@
+package inf112.skeleton.app;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BoardTests {
+
+    @Test
+    public void checkTrue() {
+        assertTrue(true);
+    }
+}
