e4903f2214ac6a513bb0bd0f870f137efa710d04
Erik Hystad <E.Hystad@student.uib.no>
2020-02-06 18:57:14 +0100

    Test

--- src/test/java/inf112/skeleton/app/BoardTests.java
+++ src/test/java/inf112/skeleton/app/BoardTests.java
@@ -2,15 +2,18 @@ package inf112.skeleton.app;
 
 import static org.junit.Assert.*;
 
+import inf112.skeleton.app.objects.Robot;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BoardTests {
-    public Board board;
+    private Board board;
+    private Robot robot;
 
     @Before
     public void setUp(){
         board = new Board(12, 12);
+        robot = new Robot(Direction.NORTH);
     }
 
     @Test
@@ -20,7 +23,10 @@ public class BoardTests {
 
     @Test
     public void checkMoveDir(){
-
+        board.addObject(robot, 0, 0);
+        int prevX = robot.getTileX();
+        board.moveObjectDir(robot, robot.getDirection());
+        assertNotEquals(prevX, robot.getTileX());
     }
 
 }
