0ee9d868466a41d40f0464bbf81d38fadbb88cd1
Tord Johansson <tord.johansson@student.uib.no>
2020-02-14 11:25:06 +0100

    added some tests to CardTest.java

--- src/test/java/inf112/skeleton/app/CardTest.java
+++ src/test/java/inf112/skeleton/app/CardTest.java
@@ -5,6 +5,25 @@ import org.junit.Test;
 import inf112.skeleton.app.objects.Card;
 
 public class CardTest {
-    
+    Card testCard;
+    @Before
+    public void setUp(){
+        testCard = new Card();
+
+    }
+
+
+    public void isCardNameEqualToGetCardName(){
+
+    }
+
+
+    public void isCardValueEqualToGetCardValue(){
+
+    }
+
+
+
+
 
 }
