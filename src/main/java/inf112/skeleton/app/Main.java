12418cf47b6dedde4f75ada58e64589f332ac9e4
KrisAesoey <kris.aesoey@gmail.com>
2020-02-25 11:41:43 +0100

    Added settings for final variables like screensize

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "RoboRally :)";
-        cfg.width = 384;
-        cfg.height = 512;
+        cfg.width = Settings.SCREEN_WIDTH;
+        cfg.height = Settings.SCREEN_HEIGHT;
         cfg.resizable = false; //temporarly
         //new LwjglApplication(new HelloWorld());
         new LwjglApplication(new Game(), cfg);
---
+++ src/main/java/inf112/skeleton/app/Settings.java
@@ -0,0 +1,9 @@
+package inf112.skeleton.app;
+
+public class Settings {
+    static final int SCREEN_WIDTH = 1280;
+    static final int SCREEN_HEIGHT = 720;
+
+    static final int BOARD_WIDTH = 512;
+    static final int BOARD_HEIGHT = 384;
+}
