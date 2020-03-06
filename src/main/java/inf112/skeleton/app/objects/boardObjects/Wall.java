5fbd47da6bd319ab1f8eeb75266154087ed7938b
Tord Johansson <tordjoha1@gmail.com>
2020-03-06 12:22:55 +0100

    filled out some method in Wall.java

--- src/main/java/inf112/skeleton/app/objects/boardObjects/Wall.java
+++ src/main/java/inf112/skeleton/app/objects/boardObjects/Wall.java
@@ -3,16 +3,18 @@ package inf112.skeleton.app.objects.boardObjects;
 import inf112.skeleton.app.board.Direction;
 
 public class Wall implements IBoardObject {
-
+    private Direction direction;
+    String tileX;
+    String tileY;
 
     @Override
     public Direction getDirection() {
-        return null;
+        return direction;
     }
 
     @Override
     public void setDirection(Direction d) {
-
+        direction = d;
     }
 
     @Override
