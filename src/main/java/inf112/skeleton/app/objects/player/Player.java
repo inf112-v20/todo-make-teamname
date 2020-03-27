c486ea830fb0433927b0104d7c53bf62330105a7
madsnesse <mbnesse@hotmail.com>
2020-03-27 09:21:40 +0100

    Merge branch 'mads'-mindfull-madness' of C:\Users\mbnes\IdeaProjects\todo-make-teamname with conflicts.

--- src/main/java/inf112/skeleton/app/objects/player/Player.java
+++ src/main/java/inf112/skeleton/app/objects/player/Player.java
@@ -1,12 +1,18 @@
 package inf112.skeleton.app.objects.player;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.Queue;
+import inf112.skeleton.app.objects.boardObjects.Flag;
 import inf112.skeleton.app.objects.cards.ProgramCard;
+
+import java.util.ArrayList;
 
 public class Player {
     private Robot robot;
     public ProgramCard[] cards;
     private Queue<ProgramCard> selectedCards;
+    private ArrayList<Flag> flags;
+    private boolean readyButtonPuhed = false;
 
     public Player(){
         robot = new Robot();
@@ -14,18 +20,22 @@ public class Player {
         robot.setTileX(0);
         robot.setTileY(0);
         selectedCards  = new Queue<ProgramCard>();
+        flags = new ArrayList<>();
     }
 
     public boolean hasWon(){return true;}
     public void deal(){
         //TODO:: change so that players get random cards
+        Texture[] images = new Texture[]{new Texture("assets/cards/card_move_1.png"),
+                new Texture("assets/cards/card_move_2.png"),
+                new Texture("assets/cards/card_turn_left.png"),
+                new Texture("assets/cards/card_turn_right.png")};
         cards = new ProgramCard[5];
-        cards[0] = (new ProgramCard(1,false,false,false));
-
-        cards[1] = (new ProgramCard(1,false,false,false));
-        cards[2]= (new ProgramCard(2,false,false,false));
-        cards[3] = (new ProgramCard(0,true,true,false));
-        cards[4] = (new ProgramCard(0,true,false,true));
+        cards[0] = (new ProgramCard(1,false,false,false, images));
+        cards[1] = (new ProgramCard(1,false,false,false, images));
+        cards[2]= (new ProgramCard(2,false,false,false, images));
+        cards[3] = (new ProgramCard(0,true,true,false, images));
+        cards[4] = (new ProgramCard(0,true,false,true, images));
     }
     public void setCards(ProgramCard[] cards){this.cards = cards;}
     public ProgramCard[] getCards(){return cards;}
@@ -67,11 +77,26 @@ public class Player {
         return robot.getLife();
     }
 
-    public Queue<ProgramCard> copySelected() {
-        Queue<ProgramCard> newHand = new Queue<>();
-        for (ProgramCard card : selectedCards) {
-            newHand.addLast(card);
+
+    public ProgramCard[] getArrayCards(){
+        ProgramCard[] newHand = new ProgramCard[selectedCards.size];
+        for (int i = 0; i < selectedCards.size; i++) {
+            newHand[i] = selectedCards.get(i);
         }
         return newHand;
+    }
+
+    public ArrayList<Flag> getFlags() {
+        return flags;
+    }
+    public void addFlag(Flag flag){
+        flags.add(flag);
+    }
+
+    public boolean getPushedReady() {
+        return this.readyButtonPuhed;
+    }
+    public void setPushedReady(boolean b){
+        this.readyButtonPuhed = b;
     }
 }
